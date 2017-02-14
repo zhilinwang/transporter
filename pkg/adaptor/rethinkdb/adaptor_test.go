b3a47888d640f33aadd3d62ed647eb67f4bd6f25
JP <jp@compose.io>
2017-02-14 16:25:14 -0600

    rethinkdb writer and tests

--- pkg/adaptor/rethinkdb/adaptor_test.go
+++ pkg/adaptor/rethinkdb/adaptor_test.go
@@ -18,7 +18,7 @@ var (
 	}
 	defaultSession *Session
 
-	dbsToTest = []*TestData{startTestData, listenTestData, readerTestData, tailTestData}
+	dbsToTest = []*TestData{startTestData, listenTestData, readerTestData, tailTestData, writerTestData}
 )
 
 type TestData struct {
